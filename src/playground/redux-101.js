209d8ec440028d1e097d874734fda86e75ac5244
Anthony Wiloughby <awilloughby@bestunion.com>
2018-02-12 17:31:51 -0500

    setting up the demo state for out expense app

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -21,7 +21,7 @@ const resetCount = () => ({
     type: 'RESET'
 });
 
-const store = createStore ((state = { count: 0 }, action) => {
+const countReducer = (state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
@@ -42,11 +42,14 @@ const store = createStore ((state = { count: 0 }, action) => {
         default:
             return state;
     };
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe( () => {
     console.log(store.getState());
 });
+
 
 store.dispatch(incrementCount({ incrementBy: 5 }));
 
---
+++ src/playground/redux-expensify.js
@@ -0,0 +1,17 @@
+import { createStore, combinedReducers } from 'redux'
+
+const demoState =  {
+    expenses: [{
+        id: 'xyz',
+        description: 'January rent',
+        note: 'this was the final payment for that address',
+        amount: 54500,
+        createdAt: 0
+    }],
+    filters: {
+        text: 'rent',
+        sortBy: 'amount', //date or amount
+        startDate: undefined,
+        endDate: undefined
+    }
+};
